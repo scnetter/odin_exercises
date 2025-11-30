1f6e2730599f3d301fa3684775a1de35c556f92d
Kelly Netterville <36lima@gmail.com>
2025-11-29 18:55:22 -0600

    Initial code to collect substrings ignoring anything not a-z

---
+++ substrings.rb
@@ -0,0 +1,35 @@
+
+
+# #substrings that takes a word as the first argument and 
+# then an array of valid substrings (your dictionary) as the 
+# second argument. It should return a hash listing each substring 
+# (case insensitive) that was found in the original string and 
+# how many times it was found.
+# 
+#
+# Examples:
+# dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+# substrings("below", dictionary)
+# => { "below" => 1, "low" => 1 }
+# 
+#  substrings("Howdy partner, sit down! How's it going?", dictionary)
+#  => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
+
+def substrings(input, dictionary)
+  # Break input into all substrings (lowcase) and create a hash
+  input_words = input.downcase.split
+  substrings = {}
+
+  input_words.each do |word|
+    word_accumulator = ""
+    word.split("").each do |char|
+      if(char.ord.between?(97, 122))
+        word_accumulator += char
+        substrings[word_accumulator] = 0
+      end
+    end
+  end
+  p substrings
+end
+
+substrings("Hello, there", ["test"])
