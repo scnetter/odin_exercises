33ac89ebe94f11a5ac7fef96c4cb6599b95a660b
Kelly Netterville <36lima@gmail.com>
2025-11-29 18:22:49 -0600

    Use modulus to handle large shifts and wrapping shifts

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,33 +1,27 @@
-require "pry"
-require "pry-byebug"
-
 LOWER_CASE_BASE = 97
 LOWER_CASE_MAX = 122
 UPPER_CASE_BASE = 65
 UPPER_CASE_MAX = 90
 
 def caesar_cipher(str, shift)
-  # A = 65, Z = 90, a = 97, z = 122, <space> = 32
-  
-  # get the current ASCII value of the characters in the string
-  original_ascii = str.split("").map { |char| char.ord }
-  
-  shifted_chars = original_ascii.map do |code|
-  
-    # Still not working fully. Need to account for shifts where it wraps (i.e. "W" with a shift of 5)
-    # Will be 92 after current calculation. Needs to be 66. 
-    if code.between?(UPPER_CASE_BASE,UPPER_CASE_MAX)
-      # binding.pry
-      (code - UPPER_CASE_BASE + shift + UPPER_CASE_BASE)
-    elsif code.between?(LOWER_CASE_BASE,LOWER_CASE_MAX)
-      (code - LOWER_CASE_BASE + shift + LOWER_CASE_BASE).chr
+  # Normalize the shift to the range 0..25 so large shifts or negatives work
+  shift = shift % 26
+
+  str.chars.map do |char|
+    code = char.ord
+
+    if code.between?(UPPER_CASE_BASE, UPPER_CASE_MAX)
+      (((code - UPPER_CASE_BASE + shift) % 26) + UPPER_CASE_BASE).chr
+    elsif code.between?(LOWER_CASE_BASE, LOWER_CASE_MAX)
+      (((code - LOWER_CASE_BASE + shift) % 26) + LOWER_CASE_BASE).chr
     else
-      code.chr
+      char
     end
-  end.join("")
+  end.join
 end
 
-puts caesar_cipher("What", 5)
+# Example:
+# puts caesar_cipher("What", 5) #=> "Bmfy"
 
 # TODO: 
 # Problems in current code
