685d00073d157867756037ebdab6a29ef1d80b61
Kelly Netterville <36lima@gmail.com>
2025-11-30 18:30:55 -0600

    Refactor to use regexp to scan for words in the dictionary

--- substrings.rb
+++ substrings.rb
@@ -1,6 +1,3 @@
-
-require "pry"
-require "pry-byebug"
 # #substrings that takes a word as the first argument and 
 # then an array of valid substrings (your dictionary) as the 
 # second argument. It should return a hash listing each substring 
@@ -17,26 +14,18 @@ require "pry-byebug"
 #  => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
 
 def substrings(input, dictionary)
-  # Break input into all substrings (lowcase) and create a hash
-  input_words = input.downcase.split
-  substrings_from_input = []
 
-  input_words.each do |word|
-    while !word.empty?
-      word_accumulator = ""
-      word.split("").each do |char|
-        if(char.ord.between?(97, 122))
-          word_accumulator += char
-          substrings_from_input << word_accumulator
-        end
-      end
-      word = word[1,word.length - 1]
-    end
-  end
-  # search for substrings in dictionary
-  results = substrings_from_input.each_with_object(Hash.new(0)) do |substr, h|
-    if dictionary.include?(substr)
-      h[substr] += 1
-    end
+  # replace non-alpha values with " " to avoid isshes with contractions and 
+  # other punctuation
+  clean_words = input.downcase.gsub(/[^a-z]/, ' ')
+ 
+  # Use the dictionary and regex instead of building a hash of individual substrings 
+  # from the input
+  
+  dictionary.each_with_object(Hash.new(0)) do |word, counts|
+    lowercase_word = word.downcase
+    pattern = Regexp.escape(lowercase_word)
+    count = clean_words.scan(/(?=#{pattern})/).length
+    counts[lowercase_word] = count if count > 0
   end
 end
