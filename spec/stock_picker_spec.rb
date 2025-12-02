6e07e11ccafaee212bd9b75ab09952d80a76111e
Kelly Netterville <36lima@gmail.com>
2025-12-02 13:28:21 -0600

    Updated test names to be specific to tested scenario.

--- spec/stock_picker_spec.rb
+++ spec/stock_picker_spec.rb
@@ -4,10 +4,10 @@ describe "stock_picker" do
   it "picks lowest day as best day to buy and highest to sell" do
     expect(stock_picker([17,3,6,9,15,8,6,1,10])).to eq([1,4])
   end
-  it "picks lowest day as best day to buy and highest to sell" do
+  it "picks correctly when lowest and highest are the last two entries" do
     expect(stock_picker([17,3,6,9,13,8,6,1,15])).to eq([7,8])
   end
-  it "picks lowest day as best day to buy and highest to sell" do
+  it "picks correctly when highest entry is first and lowest is last" do
     expect(stock_picker([17,3,6,9,8,15,6,2,1])).to eq([1,5])
   end
 end
