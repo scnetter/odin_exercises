46152fdaaf66a42f49cbb810f2760d46ba22b12a
Kelly Netterville <36lima@gmail.com>
2025-12-05 14:07:06 -0600

    Shorten range checked each time through to avoid checking already sorted entries.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,20 +1,15 @@
 def bubble_sort(unsorted_list)
   working_list = unsorted_list.dup
+  last_index = unsorted_list.length - 1
 
   begin
     unchanged = true
-    last_index = unsorted_list.length - 1
-
-    working_list.each_with_index do |value, idx|
-      
-      0.upto(last_index - 1) do |i|
-        next unless working_list[i] > working_list[i + 1]
-        working_list[i], working_list[i + 1] = working_list[i + 1], working_list[i]
-        unchanged = false
-      end
+    0.upto(last_index - 1) do |i|
+      next unless working_list[i] > working_list[i + 1]
+      working_list[i], working_list[i + 1] = working_list[i + 1], working_list[i]
+      unchanged = false
     end
+    last_index -= 1
   end until unchanged
   working_list
 end
-
-p bubble_sort([3,2,5,4])
