435ddb8659b73ae457cf817207771329f9705ce5
Kelly Netterville <36lima@gmail.com>
2025-12-05 14:02:49 -0600

    Changes to be more idiomatic using #upto

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,14 +3,14 @@ def bubble_sort(unsorted_list)
 
   begin
     unchanged = true
+    last_index = unsorted_list.length - 1
+
     working_list.each_with_index do |value, idx|
       
-      if idx != working_list.length - 1
-        if value > working_list[idx + 1]
-          working_list[idx] = working_list[idx + 1]
-          working_list[idx + 1] = value
-          unchanged = false
-        end
+      0.upto(last_index - 1) do |i|
+        next unless working_list[i] > working_list[i + 1]
+        working_list[i], working_list[i + 1] = working_list[i + 1], working_list[i]
+        unchanged = false
       end
     end
   end until unchanged
