5217f28e04f2b1cffe68008fa2f078cb30682cac
Kelly Netterville <36lima@gmail.com>
2025-12-05 16:42:27 -0600

    Refactor for linting/rubocop. Might be overkill for a small method/problem but interesting exercise.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,20 +1,26 @@
+# frozen_string_literal: true
+
 LOWER_CASE_BASE = 97
 LOWER_CASE_MAX = 122
 UPPER_CASE_BASE = 65
 UPPER_CASE_MAX = 90
 
-def caesar_cipher(str, shift)
-  shift = shift % 26
-
-  str.chars.map do |char|
-    code = char.ord
-
-    if code.between?(UPPER_CASE_BASE, UPPER_CASE_MAX)
-      (((code - UPPER_CASE_BASE + shift) % 26) + UPPER_CASE_BASE).chr
-    elsif code.between?(LOWER_CASE_BASE, LOWER_CASE_MAX)
-      (((code - LOWER_CASE_BASE + shift) % 26) + LOWER_CASE_BASE).chr
-    else
-      char
-    end
-  end.join
+def shift_code(code, shift)
+  if code.between?(UPPER_CASE_BASE, UPPER_CASE_MAX)
+    (((code - UPPER_CASE_BASE + shift) % 26) + UPPER_CASE_BASE)
+  elsif code.between?(LOWER_CASE_BASE, LOWER_CASE_MAX)
+    (((code - LOWER_CASE_BASE + shift) % 26) + LOWER_CASE_BASE)
+  else
+    code
+  end
 end
+
+def caesar_cipher(str, shift)
+  shift %= 26
+
+  # Convert characters to their ASCII codes, shift and wrap, then convert back
+  str.bytes.map { |code| shift_code(code, shift).chr }.join
+end
+
+# Example:
+# puts caesar_cipher("What", 5) # => "Bmfy"
